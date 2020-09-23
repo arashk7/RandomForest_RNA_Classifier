83ab77533d771429e4099e309dcfba9cdef70214
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2020-09-23 11:44:57 +0800

    update

--- RNA_1/rna_main.py
+++ RNA_1/rna_main.py
@@ -4,22 +4,34 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 import seaborn as sn
 import matplotlib.pyplot as plt
+import numpy as np
 
 import pandas as pd
 
-candidates = {
-    'gmat': [780, 750, 690, 710, 780, 730, 690, 720, 740, 690, 610, 690, 710, 680, 770, 610, 580, 650, 540, 590, 620,
-             600, 550, 550, 570, 670, 660, 580, 650, 760, 640, 620, 660, 660, 680, 650, 670, 580, 590, 790],
-    'gpa': [4, 3.9, 3.3, 3.7, 3.9, 3.7, 2.3, 3.3, 3.3, 1.7, 2.7, 3.7, 3.7, 3.3, 3.3, 3, 2.7, 3.7, 2.7, 2.3, 3.3, 2, 2.3,
-            2.7, 3, 3.3, 3.7, 2.3, 3.7, 3.3, 3, 2.7, 4, 3.3, 3.3, 2.3, 2.7, 3.3, 1.7, 3.7],
-    'work_experience': [3, 4, 3, 5, 4, 6, 1, 4, 5, 1, 3, 5, 6, 4, 3, 1, 4, 6, 2, 3, 2, 1, 4, 1, 2, 6, 4, 2, 6, 5, 1, 2,
-                        4, 6, 5, 1, 2, 1, 4, 5],
-    'age': [25, 28, 24, 27, 26, 31, 24, 25, 28, 23, 25, 27, 30, 28, 26, 23, 29, 31, 26, 26, 25, 24, 28, 23, 25, 29, 28,
-            26, 30, 30, 23, 24, 27, 29, 28, 22, 23, 24, 28, 31],
-    'admitted': [2, 2, 1, 2, 2, 2, 0, 2, 2, 0, 0, 2, 2, 1, 2, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 2, 0, 0, 1, 1,
-                 1, 0, 0, 0, 0, 2]
-    }
+features1 = pd.read_csv('../Dataset/Sample1.csv')
+features1.head()
+features2 = pd.read_csv('../Dataset/Sample2.csv')
+features2.head()
+features = pd.concat([features1, features2])
+features.head()
+# print(features)
 
-df = pd.DataFrame(candidates, columns=['gmat', 'gpa', 'work_experience', 'age', 'admitted'])
-print(df)
+features = features.replace('mod', 0)
+features = features.replace('unm', 1)
+features = features.replace(np.nan, 0, regex=True)
 
+# print(features)
+X = features[['q1', 'q2', 'q3', 'q4', 'q5', 'mis1', 'mis2', 'mis3', 'mis4', 'mis5']].astype(float)
+Y = features['sample'].astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, shuffle=True)
+
+clf = RandomForestClassifier(n_estimators=50)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'])
+sn.heatmap(confusion_matrix, annot=True)
+
+print('Accuracy: ', metrics.accuracy_score(y_test, y_pred))
+plt.show()
